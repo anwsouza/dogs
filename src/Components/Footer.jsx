97f55e03e30da647650ed23ec7af00457dad72e4
anassyria <anassyriamoreirasouza@gmail.com>
2023-06-14 18:26:04 -0300

    resolving error

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './Footer.module.css'
 
 export const Footer = () => {
-  return <div className={styles.header}>Footer</div>
+  return <div className={styles.header}></div>
 
 }
 
